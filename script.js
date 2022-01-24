479c3b13344d782b9e9c435bfa5365a495191da6
Ahmed Ajala <ajalaahmed8@gmail.com>
2022-01-25 04:28:42 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else{
+            $('nav').removeClass('sticky');
+        }
+    });
+
+  
+    
+    
+
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+
+      $('.js--scroll-to-food').click(function() {
+          $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+      });
+
+      $('.js--nav-icon').click(function() {
+        const nav = $('.js--main-nav');
+
+        nav.slideToggle(200);
+      })
+    
+    
+});
